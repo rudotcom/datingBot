883a8839335365e8640e3973486b33c09b671537
alex <rudotcom@gmail.com>
2021-10-21 20:07:20 +0300

    20

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import face_recognition
 import cv2
 
-import speech
+# import speech
 from utils import read_face_encodings, write_face_encoding
 from faces import Face
 
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     face_locations = []
 
     frame_count = 0
-    process_every_frame = 5
+    process_every_frame = 20
 
     # Получить ссылку на вебкамеру (#0 - камера по умолчанию)
     # image_source = 0
@@ -29,9 +29,8 @@ if __name__ == '__main__':
         if ret:
             # Обработка кадров через один для ускорения процесса
             if frame_count % process_every_frame == 0:
-                print('frame ', frame_count)
                 # Уменьшение размеров кадра для более быстрого распознавания (если что)
-                scale = 0.5
+                scale = 1
                 small_frame = cv2.resize(frame, (0, 0), fx=scale, fy=scale)
 
                 # Конвертация изображения из цветов BGR (которые испоользует OpenCV)
@@ -46,8 +45,9 @@ if __name__ == '__main__':
                     person = Face.by_encoding(face_encoding)
 
                     if person:
+                        print('frame ', frame_count, person.name)
                         if not person.recently_seen:
-                            speech.speak(f'Здравствуй, {person.name}')
+                            # speech.speak(f'Здравствуй, {person.name}')
                             print(f'Здравствуй, {person.name}')
                             person.see_you()
                     else:
