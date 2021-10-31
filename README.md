d004a1b9f4186be4eece67d08ce113e218e71e2d
coda <coda@live.ru>
2021-10-31 05:23:37 +0300

    create table if file doesn't exist

--- README.md
+++ README.md
@@ -2,16 +2,7 @@
 pip install -r requirements.txt
 ```
 
-# Создание таблицы
-import sqlite3
- 
-conn = sqlite3.connect("faces.sqlite3") # или :memory: чтобы сохранить в RAM
-cursor = conn.cursor()
- 
- 
-cursor.execute("""CREATE TABLE faces
-                  (name text, encoding BLOB)
-               """)
+
  ###############################
 cursor.execute("""DELETE FROM faces
                   WHERE name='да';
--- settings.py
+++ settings.py
@@ -1,20 +1,22 @@
 import sqlite3
+import os.path
 # ######## PATH ##########
 enc_path = 'encodings'  # Файлы с кодом лиц
 
 
 # ######## CAMERA ########
 #       Получить ссылку на вебкамеру (#0 - камера по умолчанию)
-image_source = 0
+# image_source = 0
 #       Получить ссылку на IP камеру
-# camera_ip = '192.168.1.15'
-# image_source = f'rtsp://{camera_ip}/live/ch00_0'
+camera_ip = '192.168.1.15'
+image_source = f'rtsp://{camera_ip}/live/ch00_1'
+audio_source = f'rtsp://{camera_ip}/live/ch00_0'
 
 remember_seconds = 7200
 AVATAR_NAME = 'Неваляшка'
 
 PHRASES = {
-    'hello': ['Здравствуй', 'Привет', 'Как дела', 'рад тебя видеть'],
+    'hello': ['Здравствуй', 'Привет', 'Как дела', 'рада тебя видеть'],
     'make_friends': [f'Меня зовут {AVATAR_NAME}, а тебя?', 'Как тебя зовут?',
                      f'Давай познакомимся. Меня зовут {AVATAR_NAME}, а тебя?'],
     'come_closer': ['Подойди поближе пожалуста', 'Ближе, не бойся', 'Ближе', 'Чуть ближе'],
@@ -24,6 +26,15 @@ PHRASES = {
     'positive': ['верно', 'правильно', 'да', 'конечно', 'безусловно', 'права', ],
 }
 
+db_file = "faces.sqlite3"
+if not os.path.exists(db_file):
+    # Создание таблицы бд если файла нет
+    init = sqlite3.connect(db_file)
+    cursor = init.cursor()
+    cursor.execute("CREATE TABLE faces (name text, encoding BLOB)")
+    init.commit()
+    init.close()
 
-conn = sqlite3.connect("faces.sqlite3")  # или :memory: чтобы сохранить в RAM
+conn = sqlite3.connect(db_file)  # или :memory: чтобы сохранить в RAM
 cursor = conn.cursor()
+
